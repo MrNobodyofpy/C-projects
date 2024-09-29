3960e4ab2c93ea3fe1b09cc6dd7ce8f7b5261cab
unknown <phanlehoangxzc@gmail.com>
2024-09-29 19:05:33 +0700

    Test

---
+++ Pong_Game/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Pong
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            const int fieldLength = 50; 
+            const int fieldWidth = 15;
+            const char filedTile = '#';
+            string line = string.Concat(Enumerable.Repeat(filedTile, fieldLength));
+
+            while(true)
+            {
+                Console.SetCursorPosition(0, 0);
+                Console.WriteLine(line);
+
+                Console.SetCursorPosition(0, fieldWidth);
+                Console.WriteLine(line);
+            }
+
+    }
+}
+}
